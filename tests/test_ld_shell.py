ae5a0b731b60eac221460a6960b58e8ce4ae352f
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-26 18:55:16 -0300

    Use patch as decorator on tests

--- tests/test_ld_shell.py
+++ tests/test_ld_shell.py
@@ -3,17 +3,17 @@ from unittest.mock import patch
 from nix_alien import ld_shell
 
 
-def test_create_ld_shell():
-    with patch("nix_alien.ld_shell.find_libs") as mock_find_libs:
-        mock_find_libs.return_value = {
-            "libfoo.so": "foo.out",
-            "libfoo.6.so": "foo.out",
-            "libbar.so": "bar.out",
-            "libquux.so": "quux.out",
-        }
-        assert (
-            ld_shell.create_ld_shell("xyz")
-            == """\
+@patch("nix_alien.ld_shell.find_libs")
+def test_create_ld_shell(mock_find_libs):
+    mock_find_libs.return_value = {
+        "libfoo.so": "foo.out",
+        "libfoo.6.so": "foo.out",
+        "libbar.so": "bar.out",
+        "libquux.so": "quux.out",
+    }
+    assert (
+        ld_shell.create_ld_shell("xyz")
+        == """\
 { pkgs ? import <nixpkgs> { } }:
 
 let
@@ -29,4 +29,4 @@ mkShell {
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
 }
 """
-        )
+    )
