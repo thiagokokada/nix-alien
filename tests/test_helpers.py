e38842ec456c60b1faea162ddd78e9a3bc14774e
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-27 01:03:27 -0300

    Call .expanduser() on nix_alien.helpers.get_hash_for_program()

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,7 +4,14 @@ from pathlib import Path
 from nix_alien import helpers
 
 
-def test_get_hash_for_program():
+def test_get_hash_for_program(monkeypatch):
+    monkeypatch.setenv("HOME", "/home/nameless-shelter")
+    assert helpers.get_hash_for_program("/home/nameless-shelter/abc") == UUID(
+        "d0efaea7-176f-56e6-99a0-ea49599a163b"
+    )
+    assert helpers.get_hash_for_program("~/abc") == UUID(
+        "d0efaea7-176f-56e6-99a0-ea49599a163b"
+    )
     assert helpers.get_hash_for_program("/abc") == UUID(
         "f52177f5-def5-5d9e-91fc-ef1283fc54b1"
     )
@@ -13,9 +20,6 @@ def test_get_hash_for_program():
     )
     assert helpers.get_hash_for_program("/xyz/../abc") == UUID(
         "f52177f5-def5-5d9e-91fc-ef1283fc54b1"
-    )
-    assert helpers.get_hash_for_program("/foo/bar") == UUID(
-        "aca1aca4-b9b7-5083-839b-120863df4c7d"
     )
 
 
