2b93d7efd6cf0e4b69c6e3ef1831c90df47731bb
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-26 21:21:04 -0300

    Add nix-index-update, remove overlay

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
-{ nixpkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f112b512e1710da6c8beb8e541a8ad9fcd81e6e6.tar.gz") { }
-, poetry2nix ? import (fetchTarball "https://github.com/nix-community/poetry2nix/archive/refs/tags/1.21.0.tar.gz") { pkgs = nixpkgs; poetry = nixpkgs.poetry; }
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f112b512e1710da6c8beb8e541a8ad9fcd81e6e6.tar.gz") { }
+, poetry2nix ? import (fetchTarball "https://github.com/nix-community/poetry2nix/archive/refs/tags/1.21.0.tar.gz") { inherit pkgs; poetry = pkgs.poetry; }
 }:
 
 poetry2nix.mkPoetryApplication {
--- flake.nix
+++ flake.nix
@@ -6,37 +6,54 @@
   inputs.poetry2nix.url = "github:nix-community/poetry2nix";
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    {
-      # Nixpkgs overlay providing the application
-      overlay = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: prev: {
-          # The application
-          nix-alien = import ./default.nix {
-            nixpkgs = prev;
-            inherit (prev) poetry2nix;
-          };
-        })
-      ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
+    (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
+        pkgs = import nixpkgs { inherit system; };
+        poetry2nix' = import poetry2nix {
+          inherit pkgs;
+          poetry = pkgs.poetry;
         };
       in
       rec {
-        packages.nix-alien = pkgs.nix-alien;
-        defaultPackage = self.packages.${system}.nix-alien;
+        packages = {
+          nix-alien = import ./default.nix {
+            inherit pkgs;
+            poetry2nix = poetry2nix';
+          };
+
+          nix-index-update =
+            let
+              inherit (pkgs) coreutils wget;
+            in
+            pkgs.writeShellScriptBin "nix-index-update" ''
+              set -euo pipefail
+
+              readonly filename="index-${system}"
+              readonly dest_dir="$HOME/.cache/nix-index"
+
+              ${coreutils}/bin/mkdir -p "$dest_dir"
+              pushd "$dest_dir" >/dev/null
+              trap "popd >/dev/null" EXIT
+
+              # -N will only download a new version if there is an update.
+              ${wget}/bin/wget -q -N "https://github.com/Mic92/nix-index-database/releases/latest/download/$filename"
+              ${coreutils}/bin/ln -f "$filename" files
+
+              echo "Done!"
+            '';
+        };
+
+        defaultPackage = packages.nix-alien;
 
         apps =
           let
             inherit (flake-utils.lib) mkApp;
           in
           {
-            nix-alien = mkApp { drv = pkgs.nix-alien; };
-            nix-alien-ld = mkApp { drv = pkgs.nix-alien; name = "nix-alien-ld"; };
-            nix-alien-find-libs = mkApp { drv = pkgs.nix-alien; name = "nix-alien-find-libs"; };
+            nix-alien = mkApp { drv = packages.nix-alien; };
+            nix-alien-ld = mkApp { drv = packages.nix-alien; name = "nix-alien-ld"; };
+            nix-alien-find-libs = mkApp { drv = packages.nix-alien; name = "nix-alien-find-libs"; };
+            nix-index-update = mkApp { drv = packages.nix-index-update; name = "nix-index-update"; };
           };
 
         defaultApp = apps.nix-alien;
