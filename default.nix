6148a0691affd678a1f9d58fcd8e615d49406e75
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-26 21:54:49 -0300

    Add propagatedBuildInputs to set PATH correctly

--- default.nix
+++ default.nix
@@ -2,6 +2,35 @@
 , poetry2nix ? import (fetchTarball "https://github.com/nix-community/poetry2nix/archive/refs/tags/1.21.0.tar.gz") { inherit pkgs; poetry = pkgs.poetry; }
 }:
 
-poetry2nix.mkPoetryApplication {
-  projectDir = ./.;
-}
+let
+  app = poetry2nix.mkPoetryApplication {
+    projectDir = ./.;
+
+    overrides = poetry2nix.overrides.withDefaults (
+      final: prev: {
+        pylddwrap = prev.pylddwrap.overrideAttrs (oldAttrs: {
+          propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or [ ]) ++ [
+            pkgs.glibc
+          ];
+        });
+        pyfzf = prev.pyfzf.overrideAttrs (oldAttrs: {
+          propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or [ ]) ++ [
+            pkgs.fzf
+          ];
+        });
+      }
+    );
+
+    meta = with pkgs.lib; {
+      description = "Run unpatched binaries on Nix/NixOS";
+      homepage = "https://github.com/thiagokokada/nix-alien";
+      license = licenses.mit;
+      platforms = platforms.all;
+    };
+  };
+in
+app.overrideAttrs (oldAttrs: {
+  propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or [ ]) ++ [
+    pkgs.nix-index
+  ];
+})
