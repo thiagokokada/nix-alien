a9bf86f1ea411c4050802a3583853f956c4d3fed
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-31 13:54:59 -0300

    Add tests to nix_alien.helpers.get_dest_path()

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -33,3 +33,36 @@ def test_get_cache_path(monkeypatch):
     assert helpers.get_cache_path("/abc") == Path(
         "/nix-alien/f52177f5-def5-5d9e-91fc-ef1283fc54b1"
     )
+
+
+def test_get_dest_path(monkeypatch):
+    monkeypatch.setenv("HOME", "/home/nameless-shelter")
+
+    assert helpers.get_dest_path(
+        destination=None,
+        program="/bin/foo",
+        directory="bar",
+        filename="foo.nix",
+    ) == Path(
+        "/home/nameless-shelter/.cache/nix-alien/871184f9-aad6-5705-bdb5-6f10f378d3df/bar/foo.nix"
+    )
+
+    monkeypatch.setenv("XDG_CACHE_HOME", "/")
+    assert helpers.get_dest_path(
+        destination=None,
+        program="/bin/foo",
+        directory="bar",
+        filename="foo.nix",
+    ) == Path(
+        "/nix-alien/871184f9-aad6-5705-bdb5-6f10f378d3df/bar/foo.nix"
+    )
+
+    assert (
+        helpers.get_dest_path(
+            destination="/quux",
+            program="/bin/foo",
+            directory="bar",
+            filename="foo.nix",
+        )
+        == Path("/quux/foo.nix")
+    )
