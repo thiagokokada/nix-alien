1d25569f7bab921a8496f2fed6b3e9be409fa832
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-31 00:10:31 -0300

    Fix help descriptions in nix_alien.nix_ld

--- nix_alien/nix_ld.py
+++ nix_alien/nix_ld.py
@@ -8,14 +8,14 @@ from string import Template
 from .libs import get_unique_packages, find_libs
 from .helpers import get_cache_path
 
-SHELL_TEMPLATE = Template(read_text(__package__, "nix_ld.template.nix"))
+NIX_LD_TEMPLATE = Template(read_text(__package__, "nix_ld.template.nix"))
 
 
 def create_nix_ld_drv(program: str) -> str:
     path = Path(program).expanduser()
     libs = find_libs(path)
 
-    return SHELL_TEMPLATE.safe_substitute(
+    return NIX_LD_TEMPLATE.safe_substitute(
         __name__=path.name,
         __packages__=("\n" + 4 * " ").join(get_unique_packages(libs)),
         __program__=path.absolute(),
@@ -28,14 +28,14 @@ def main(args=sys.argv[1:]):
     parser.add_argument(
         "-r",
         "--recreate",
-        help="Recreate 'shell.nix' file if exists",
+        help="Recreate 'default.nix' file if exists",
         action="store_true",
     )
     parser.add_argument(
         "-d",
         "--destination",
         metavar="PATH",
-        help="Path where 'shell.nix' file will be created",
+        help="Path where 'default.nix' file will be created",
     )
     parser.add_argument(
         "args",
