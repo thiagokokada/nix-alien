829f19400e15e32d8cc3f1ca434b3de2b24ad0a0
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-31 00:48:26 -0300

    Add --flake to nix_alien.fhs_env

--- nix_alien/fhs_env.py
+++ nix_alien/fhs_env.py
@@ -3,12 +3,14 @@ import subprocess
 import sys
 from importlib.resources import read_text
 from pathlib import Path
+from platform import machine
 from string import Template
 
 from .libs import get_unique_packages, find_libs
 from .helpers import get_cache_path
 
 FHS_TEMPLATE = Template(read_text(__package__, "fhs_env.template.nix"))
+FHS_FLAKE_TEMPLATE = Template(read_text(__package__, "fhs_env_flake.template.nix"))
 
 
 def create_fhs_env_drv(program: str) -> str:
@@ -19,6 +21,18 @@ def create_fhs_env_drv(program: str) -> str:
         __name__=path.name,
         __packages__=("\n" + 4 * " ").join(get_unique_packages(libs)),
         __program__=path.absolute(),
+    )
+
+
+def create_fhs_env_drv_flake(program: str) -> str:
+    path = Path(program).expanduser()
+    libs = find_libs(path)
+
+    return FHS_FLAKE_TEMPLATE.safe_substitute(
+        __name__=path.name,
+        __packages__=("\n" + 12 * " ").join(get_unique_packages(libs)),
+        __program__=path.absolute(),
+        __system__=f"{machine()}-linux",
     )
 
 
@@ -38,37 +52,63 @@ def main(args=sys.argv[1:]):
         help="Path where 'default.nix' file will be created",
     )
     parser.add_argument(
+        "-f",
+        "--flake",
+        help="Create and use 'flake.nix' file instead (experimental)",
+        action="store_true",
+    )
+    parser.add_argument(
         "args",
         nargs=argparse.REMAINDER,
         help="Arguments to be passed to the program",
     )
-
     parsed_args = parser.parse_args(args=args)
-    if parsed_args.destination:
-        destination = (
-            Path(parsed_args.destination).expanduser().resolve() / "default.nix"
-        )
+
+    # TODO: this code is a mess, refactor it
+    if parsed_args.flake:
+        filename = "flake.nix"
     else:
-        destination = get_cache_path(parsed_args.program) / "fhs-env/default.nix"
+        filename = "default.nix"
+
+    if parsed_args.destination:
+        destination = Path(parsed_args.destination).expanduser().resolve() / filename
+    else:
+        destination = get_cache_path(parsed_args.program) / "fhs-env" / filename
 
     if parsed_args.recreate:
         destination.unlink(missing_ok=True)
 
     if not destination.exists():
         destination.parent.mkdir(parents=True, exist_ok=True)
-        fhs_shell = create_fhs_env_drv(parsed_args.program)
+        if parsed_args.flake:
+            fhs_shell = create_fhs_env_drv_flake(parsed_args.program)
+        else:
+            fhs_shell = create_fhs_env_drv(parsed_args.program)
         with open(destination, "w") as f:
             f.write(fhs_shell)
         print(f"File '{destination}' created successfuly!")
 
-    build_path = Path(
+    if parsed_args.flake:
         subprocess.run(
-            ["nix-build", "--no-out-link", destination],
-            check=True,
-            capture_output=True,
-            text=True,
-        ).stdout.strip()
-    )
+            [
+                "nix",
+                "run",
+                "--experimental-features",
+                "nix-command flakes",
+                destination.parent,
+                "--",
+                *parsed_args.args,
+            ]
+        )
+    else:
+        build_path = Path(
+            subprocess.run(
+                ["nix-build", "--no-out-link", destination],
+                check=True,
+                capture_output=True,
+                text=True,
+            ).stdout.strip()
+        )
 
-    name = Path(parsed_args.program).name
-    subprocess.run([build_path / "bin" / f"{name}-fhs", *parsed_args.args])
+        name = Path(parsed_args.program).name
+        subprocess.run([build_path / "bin" / f"{name}-fhs", *parsed_args.args])
--- tests/test_fhs_env.py
+++ tests/test_fhs_env.py
@@ -34,6 +34,55 @@ buildFHSUserEnv {
     )
 
 
+@patch("nix_alien.fhs_env.find_libs")
+@patch("nix_alien.nix_ld.machine")
+def test_create_fhs_env_flake(mock_machine, mock_find_libs):
+    mock_machine.return_value = "x86_64"
+    mock_find_libs.return_value = {
+        "libfoo.so": "foo.out",
+        "libfoo.6.so": "foo.out",
+        "libbar.so": "bar.out",
+        "libquux.so": "quux.out",
+    }
+    assert (
+        fhs_env.create_fhs_env_drv_flake("xyz")
+        == """\
+{
+  description = "xyz-fhs";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    rec {
+      defaultPackage.${system} =
+        let
+          inherit (pkgs) buildFHSUserEnv;
+        in
+        buildFHSUserEnv {
+          name = "xyz-fhs";
+          targetPkgs = p: with p; [
+            bar.out
+            foo.out
+            quux.out
+          ];
+          runScript = "%s/xyz";
+        };
+
+      defaultApp.${system} = {
+        type = "app";
+        program = "${defaultPackage.${system}}/bin/xyz-fhs";
+      };
+    };
+}
+"""
+        % Path(__file__).parent.parent.absolute()
+    )
+
+
 @patch("nix_alien.fhs_env.subprocess")
 @patch("nix_alien.fhs_env.find_libs")
 def test_main_wo_args(mock_find_libs, mock_subprocess, monkeypatch, tmp_path):
