035488b4727492a7ab25e118a6203f32b3ec87e8
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-04-23 00:38:56 +0100

    Still need this workaround

--- shell.nix
+++ shell.nix
@@ -12,6 +12,17 @@ let
       nix-alien = ./nix-alien;
       tests = ./tests;
     };
+
+    overrides = poetry2nix.overrides.withDefaults (
+      final: prev: {
+        # Fix conflict between icontract and pylddwrap files
+        icontract = prev.icontract.overrideAttrs (oldAttrs: {
+          postInstall = (oldAttrs.postInstall or "") + ''
+            rm -f $out/*.rst $out/*.txt
+          '';
+        });
+      }
+    );
   };
 in
 appEnv.env.overrideAttrs (oldAttrs: {
