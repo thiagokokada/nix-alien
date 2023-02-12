030988ed828036e82a93c99f651cd6b40e4f952e
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-02-12 15:56:49 +0000

    Use flake-compat in compat.nix

--- default.nix
+++ default.nix
@@ -1,28 +1,16 @@
 { pkgs ? (import ./compat.nix).pkgs
-, self ? { }
+, self ? (import ./compat.nix).flake
 }:
 
 let
   inherit (pkgs) lib;
-  flake =
-    if (self == { }) then
-      (import
-        (
-          let lock = with builtins; fromJSON (readFile ./flake.lock); in
-          builtins.fetchTarball {
-            url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-            sha256 = lock.nodes.flake-compat.locked.narHash;
-          }
-        )
-        { src = ./.; }).defaultNix
-    else self;
-  dirty = lib.optionalString (!(flake ? rev)) "_dirty";
-  rev = "0.1.0+git${flake.lastModifiedDate}${dirty}";
+  dirty = lib.optionalString (!(self ? rev)) "_dirty";
+  rev = "0.1.0+git${self.lastModifiedDate}${dirty}";
 in
 {
   nix-alien = pkgs.callPackage ./nix-alien.nix {
     inherit rev;
-    nix-index = flake.inputs.nix-index-database.packages.${pkgs.system}.nix-index-with-db;
+    nix-index = self.inputs.nix-index-database.packages.${pkgs.system}.nix-index-with-db;
   };
 
   # For backwards compat, may be removed in the future
