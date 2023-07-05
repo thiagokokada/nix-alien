29b969f5a22d29dddd4da0917e9f18a8aacd85de
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-07-05 23:08:20 +0100

    Remove almost all functions from __all__
    I just broke the API, I don't really want to maintain a stable API
    and I also don't think anyone is using this as a library (it is not even
    published somewhere interesting like PyPi).
    
    So let's remove ilusion that we have a public API. If you still want to
    do so you can of course, but this at your risk now.

--- nix_alien/__init__.py
+++ nix_alien/__init__.py
@@ -1,28 +1,3 @@
 from ._version import __version__
-from .fhs_env import (
-    create_fhs_env,
-    create_fhs_env_drv,
-    create_fhs_env_drv_flake,
-    create_fhs_env_flake,
-)
-from .libs import find_lib_candidates, find_libs
-from .nix_ld import (
-    create_nix_ld,
-    create_nix_ld_drv,
-    create_nix_ld_drv_flake,
-    create_nix_ld_flake,
-)
 
-__all__ = [
-    "__version__",
-    "create_fhs_env",
-    "create_fhs_env_drv",
-    "create_fhs_env_drv_flake",
-    "create_fhs_env_flake",
-    "create_nix_ld",
-    "create_nix_ld_drv",
-    "create_nix_ld_drv_flake",
-    "create_nix_ld_flake",
-    "find_lib_candidates",
-    "find_libs",
-]
+__all__ = ["__version__"]
