910c5480f03d065bb821f1eb6d41698b92b4b2da
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-10-25 22:35:08 +0100

    Improve integration test reporting

--- integration-tests.nix
+++ integration-tests.nix
@@ -12,24 +12,36 @@ let
     url = "https://github.com/babashka/babashka/releases/download/v${babashkaVersion}/babashka-${babashkaVersion}-linux-amd64.tar.gz";
     hash = "sha256-2WzGw0GxJpL3owiSf24moZvAeQ8TKFSul1PRvKS3OWI=";
   };
+  setup = ''
+    export PATH="${lib.makeBinPath [ nix-alien gnugrep ]}:$PATH"
+    export HOME="$(mktemp -d)"
+    . /etc/os-release
+  '';
+  reportResult = ''
+    if [[ "$?" == 0 ]]; then
+      echo "[TEST RESULT] Success!"
+    else
+      echo "[TEST RESULT] Failure!"
+    fi
+  '';
 in
 {
   nix-alien = pkgs.writeShellScriptBin "nix-alien-it" ''
-    export PATH="${lib.makeBinPath [ nix-alien gnugrep ]}:$PATH"
-    export HOME="$(mktemp -d)"
+    ${setup}
     nix-alien -c zlib.out ${babashka}/bb -- --version | grep -F "babashka v${babashkaVersion}"
+    ${reportResult}
   '';
 
   nix-alien-flake = pkgs.writeShellScriptBin "nix-alien-flake-it" ''
-    export PATH="${lib.makeBinPath [ nix-alien gnugrep ]}:$PATH"
-    export HOME="$(mktemp -d)"
+    ${setup}
     nix-alien -c zlib.out --flake ${babashka}/bb -- --version | grep -F "babashka v${babashkaVersion}"
+    ${reportResult}
   '';
 
   nix-alien-find-libs = pkgs.writeShellScriptBin "nix-alien-find-libs-it" ''
-    export PATH="${lib.makeBinPath [ nix-alien gnugrep ]}:$PATH"
-    export HOME="$(mktemp -d)"
+    ${setup}
     nix-alien-find-libs -c zlib.out ${babashka}/bb | grep -F "zlib.out"
+    ${reportResult}
   '';
 
   # The tests below will fail in a NixOS system without NIX_LD setup, so we
@@ -39,27 +51,24 @@ in
   # will just re-use the system libs (and almost every system contains zlib
   # installed).
   nix-alien-ld = pkgs.writeShellScriptBin "nix-alien-ld-it" ''
-    . /etc/os-release
-    export PATH="${lib.makeBinPath [ nix-alien gnugrep ]}:$PATH"
-    export HOME="$(mktemp -d)"
-
+    ${setup}
     if [[ "$NAME" == "NixOS" ]] && [[ -z "$NIX_LD" ]]; then
       echo "[WARN] NIX_LD not setup! Will only test nix evaluation."
       nix-alien-ld -c zlib.out ${babashka}/bb -- --version 2>&1 | grep -F "required file not found"
     else
       nix-alien-ld -c zlib.out ${babashka}/bb -- --version | grep -F "babashka v${babashkaVersion}"
     fi
+    ${reportResult}
   '';
 
   nix-alien-ld-flake = pkgs.writeShellScriptBin "nix-alien-ld-flake-it" ''
-    . /etc/os-release
-    export PATH="${lib.makeBinPath [ nix-alien gnugrep ]}:$PATH"
-    export HOME="$(mktemp -d)"
+    ${setup}
     if [[ "$NAME" == "NixOS" ]] && [[ -z "$NIX_LD" ]]; then
       echo "[WARN] NIX_LD not setup! Will only test nix evaluation."
       nix-alien-ld -c zlib.out --flake ${babashka}/bb -- --version 2>&1 | grep -F "required file not found"
     else
       nix-alien-ld -c zlib.out --flake ${babashka}/bb -- --version | grep -F "babashka v${babashkaVersion}"
     fi
+    ${reportResult}
   '';
 }
